d5899018b17c3a7fb2ad1c9424c0e1e580bd3037
Segolene-Albouy <segolene.albouy@gmail.com>
2024-09-27 11:49:00 +0200

    [SCRIPT] clean to kill every process on Ctrl+C

--- run.sh
+++ run.sh
@@ -1,7 +1,26 @@
-#! /bin/bash
+#!/bin/bash
+
 [ ! -f .env.dev ] || export $(grep -v '^#' .env.dev | xargs)
 
-(trap 'kill 0' SIGINT;
-    (export CUDA_VISIBLE_DEVICES=$DEVICE_NB && venv/bin/flask --app app.main run --debug -p $DEV_PORT) &
-    (venv/bin/dramatiq app.main -t 1 -p 1) &
-);
+flask_pid=""
+dramatiq_pid=""
+
+cleanup() {
+    echo "Shutting down processes..."
+    [ -n "$flask_pid" ] && kill "$flask_pid"
+    [ -n "$dramatiq_pid" ] && kill "$dramatiq_pid"
+    wait
+    echo "All processes terminated."
+    exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+export CUDA_VISIBLE_DEVICES=$DEVICE_NB
+venv/bin/flask --app app.main run --debug -p "$DEV_PORT" &
+flask_pid=$!
+
+venv/bin/dramatiq app.main -t 1 -p 1 &
+dramatiq_pid=$!
+
+wait
